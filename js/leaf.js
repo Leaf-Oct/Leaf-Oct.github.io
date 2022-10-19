33fc26f8524ab55a896440a64ac47d25ca236966
十月叶 <74179287+Leaf-Oct@users.noreply.github.com>
2022-10-19 21:02:40 +0800

    Delete js directory

--- js/leaf.js
+++
@@ -1,7 +0,0 @@
-// 将其它文件的字符输入到当前页面的方法,来自StackOverflow, 感谢🙇‍大佬
-function printFile(file){
-	var rawFile=new XMLHttpRequest();
-	rawFile.open("GET",file,false);
-	rawFile.send(null);
-	return rawFile.responseText;
-}
